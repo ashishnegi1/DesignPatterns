92e5c29ec474c868cd574cc5df9738d257d9763a
Ashish Negi <myasish.negi@gmail.com>
2024-08-14 16:16:09 +0530

    Learned Singleton pattern

---
+++ src/main/java/SingletonPattern/LimitedEditionCarSingletonPattern.java
@@ -0,0 +1,16 @@
+package SingletonPattern;
+
+public class LimitedEditionCarSingletonPattern {
+
+    private static LimitedEditionCarSingletonPattern oneOfOneGT = new LimitedEditionCarSingletonPattern();
+
+    private LimitedEditionCarSingletonPattern(){}
+
+    static LimitedEditionCarSingletonPattern getInstance(){
+        return oneOfOneGT;
+    }
+
+    public void run(){
+        System.out.println("This is a 1 of 1 GT car running.");
+    }
+}
